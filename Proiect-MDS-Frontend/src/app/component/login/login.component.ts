91649a07f3cecb0e57b45414ecb4690ce29502bf
Modreanu Maria <maria.modreanu@s.unibuc.ro>
2024-04-23 23:31:33 +0300

    login register implementation

--- Proiect-MDS-Frontend/src/app/component/login/login.component.ts
+++ Proiect-MDS-Frontend/src/app/component/login/login.component.ts
@@ -1,53 +1,62 @@
-import { Component,inject,OnInit } from '@angular/core';
-import { RouterOutlet,Router} from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HttpClient,HttpClientModule} from '@angular/common/http';
-import { FormBuilder,FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  imports: [CommonModule,RouterOutlet,HttpClientModule,ReactiveFormsModule],
-  providers:[UserService],
-  styleUrls: ['./login.component.css'], 
-  standalone: true
+  imports: [CommonModule, RouterOutlet, HttpClientModule, ReactiveFormsModule],
+  providers: [UserService],
+  styleUrls: ['./login.component.css'],
+  standalone: true,
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  errorMessage="";
-  constructor(private fb: FormBuilder, private httpClient: HttpClient,private router:Router,private loginService:UserService,private cookieService:CookieService) {
-    this.loginForm=this.fb.group({
-      userName: ['',Validators.required],
-      parola: ['',[Validators.required,Validators.minLength(8)]],
-      remember: [false,Validators.required]
+  errorMessage = '';
+  constructor(
+    private fb: FormBuilder,
+    private httpClient: HttpClient,
+    public router: Router,
+    private loginService: UserService,
+    private cookieService: CookieService
+  ) {
+    this.loginForm = this.fb.group({
+      userName: ['', Validators.required],
+      parola: ['', [Validators.required, Validators.minLength(8)]],
+      remember: [false, Validators.required],
     });
   }
   login() {
     console.log(this.loginForm.value);
-    this.loginService.login(this.loginForm.value).subscribe(
 
-      (response: any) => {
-        console.log(response);
-        this.cookieService.set
-        ('token', response.token, undefined, '/', undefined, false, "Strict");
-        this.errorMessage = 'Conectat';
-        this.router.navigate(["landingPage"]);
-      },
-      // (error: any) => {
-      //   console.error(error);
-      //   this.errorMessage = error.error;
-      // }
-    );
-  }
-  ngOnInit(){ 
-    this.loginForm=this.fb.group({
-      userName: ['',Validators.required],
-      parola: ['',[Validators.required,Validators.minLength(8)]],
-      remember: [false,Validators.required]
+    this.loginService.login(this.loginForm.value).subscribe((response: any) => {
+      console.log(response);
+      this.cookieService.set(
+        'token',
+        response.token,
+        undefined,
+        '/',
+        undefined,
+        false,
+        'Strict'
+      );
+      this.errorMessage = 'Conectat';
+      this.router.navigate(['landingPage']);
     });
   }
-  register(){
-    alert("de implementat");
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      userName: ['', Validators.required],
+      parola: ['', [Validators.required, Validators.minLength(8)]],
+      remember: [false, Validators.required],
+    });
   }
 }
--- Proiect-MDS-Frontend/src/app/component/register/register.component.ts
+++ Proiect-MDS-Frontend/src/app/component/register/register.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, ReactiveFormsModule],
+  providers: [UserService],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  errorMessage = '';
+  constructor(
+    private fb: FormBuilder,
+    private httpClient: HttpClient,
+    public router: Router,
+    private registerService: UserService,
+    private cookieService: CookieService
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      parola: ['', [Validators.required, Validators.minLength(8)]],
+      nume: ['', Validators.required],
+      prenume: ['', Validators.required],
+      email: ['', Validators.required],
+      nrTelefon: ['', [Validators.required, Validators.maxLength(15)]],
+    });
+  }
+  register() {
+    console.log(this.registerForm.value);
 
+    this.registerService
+      .register(this.registerForm.value)
+      .subscribe((response: any) => {
+        console.log(response);
+        this.registerService
+          .login(this.registerForm.value)
+          .subscribe((loginResponse) => {
+            this.cookieService.set(
+              'token',
+              loginResponse.token,
+              undefined,
+              '/',
+              undefined,
+              false,
+              'Strict'
+            );
+            this.errorMessage = 'Conectat';
+            this.router.navigate(['landingPage']);
+          });
+      });
+  }
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      parola: ['', [Validators.required, Validators.minLength(8)]],
+      nume: ['', Validators.required],
+      prenume: ['', Validators.required],
+      email: ['', Validators.required],
+
+      nrTelefon: ['', [Validators.required, Validators.maxLength(15)]],
+      //remember: [false, Validators.required],
+    });
+  }
+  // register() {
+  //   alert('de implementat');
+  // }
 }
