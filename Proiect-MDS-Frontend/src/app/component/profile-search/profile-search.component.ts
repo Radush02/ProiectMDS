2e9319624f8e354c64de05cef2bd84c78cac8791
Modreanu Maria <maria.modreanu@s.unibuc.ro>
2024-05-13 17:34:53 +0300

    Added profile-search to navbar

---
+++ Proiect-MDS-Frontend/src/app/component/profile-search/profile-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-search.component.html',
+  styleUrl: './profile-search.component.css'
+})
+export class ProfileSearchComponent {
+
+}
