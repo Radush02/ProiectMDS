07ff4510c4155f85d28062a58499d939d4796218
Modreanu Maria <maria.modreanu@s.unibuc.ro>
2024-05-13 19:16:28 +0300

    Added the search username feature

--- Proiect-MDS-Frontend/src/app/component/profile-search/profile-search.component.ts
+++ Proiect-MDS-Frontend/src/app/component/profile-search/profile-search.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { UserService } from '../../services/user.service';
+import { S3Service } from '../../services/s3.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-profile-search',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, CommonModule],
+  providers: [UserService, S3Service],
   templateUrl: './profile-search.component.html',
-  styleUrl: './profile-search.component.css'
+  styleUrls: ['./profile-search.component.css'],
 })
 export class ProfileSearchComponent {
+  username = '';
+  errorMessage = '';
+  constructor(
+    public router: Router,
+    private cookieService: CookieService,
+    private userService: UserService,
+    private s3Service: S3Service
+  ) {}
 
+  ngOnInit(): void {
+    if (this.cookieService.get('token') === '') {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  searchProfile(username: string) {
+    if (username) {
+      this.userService.getUser(username).subscribe(
+        (result) => {
+          this.router.navigate(['/profile'], {
+            queryParams: { user: username },
+          });
+        },
+        (error) => {
+          console.error('User not found:', error);
+          if (error.status === 404) {
+            this.errorMessage = 'User not found.';
+          } else {
+            this.errorMessage =
+              'An error occurred while searching for the user.';
+          }
+        }
+      );
+    }
+  }
 }
